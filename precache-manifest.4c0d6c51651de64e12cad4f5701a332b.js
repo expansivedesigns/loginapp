258be22f1bbd174974d5566ec9718de5ad5bf1fe
TaLoveous Goodwin <TaLoveous@expansivedesigns.com>
2019-12-14 21:41:06 -0500

    Updates

---
+++ precache-manifest.4c0d6c51651de64e12cad4f5701a332b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f950d88bc4f54a276b69e9bc4cf0427",
+    "url": "/loginapp/index.html"
+  },
+  {
+    "revision": "d17ace4810a39bc59c84",
+    "url": "/loginapp/static/css/main.cfca6d36.chunk.css"
+  },
+  {
+    "revision": "75e69260cb93a12a39d2",
+    "url": "/loginapp/static/js/2.2933d490.chunk.js"
+  },
+  {
+    "revision": "d17ace4810a39bc59c84",
+    "url": "/loginapp/static/js/main.cdcba069.chunk.js"
+  },
+  {
+    "revision": "e07be09ecbdb555dab3d",
+    "url": "/loginapp/static/js/runtime-main.288b62e8.js"
+  }
+]);
